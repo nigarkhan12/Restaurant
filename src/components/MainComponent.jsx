a0c01c9fbe8eb5d1deed329daa6bde72255fbe23
nigarkhan12 <nigarkhan92@gmail.com>
2022-10-25 17:31:32 +0530

    body, header, footer, navigation files added

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "./body/body";
+import Footer from "./footer/footer";
+import Header from "./header/header";
+
+const MainComponent = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+export default MainComponent;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "./navigation";
+
+const Header = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+};
+
+export default Header;
