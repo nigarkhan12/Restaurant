11143e61dca7a675ee15e98636f63bd23336e4cd
nigarkhan12 <nigarkhan92@gmail.com>
2022-11-01 22:12:37 +0530

    preparing project for Router

---
+++ src/components/body/contact.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class contact extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contact Page</h1>
+      </div>
+    );
+  }
+}
+
+export default contact;
---
+++ src/components/body/home.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class home extends Component {
+  render() {
+    return <div>home</div>;
+  }
+}
+
+export default home;
