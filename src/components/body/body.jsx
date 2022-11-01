02dfaeda6799aecb71228548d924d5ce8aaa3230
nigarkhan12 <nigarkhan92@gmail.com>
2022-11-01 23:24:03 +0530

    routes added

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Menu from "./menu";
+import About from "./about";
+import Contact from "./contact";
+import Home from "./home";
+import { Routes, Route } from "react-router-dom";
 
 const Body = () => {
   return (
     <div>
-      <Menu />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/menu" element={<Menu />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 };
